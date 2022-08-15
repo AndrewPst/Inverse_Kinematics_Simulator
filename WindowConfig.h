a30bc2d173eaccd78bf613db8c8a3bd8f88bca75
Andrew Pustovit <AndrewPstvt@yandex.ru>
2022-08-15 20:38:02 +0300

    Add the window config file

---
+++ WindowConfig.h
@@ -0,0 +1,12 @@
+#pragma once
+
+constexpr int W_WIDTH = 1920;
+constexpr int W_HEIGHT = 1080;
+
+constexpr int W_START_X = 100;
+constexpr int W_START_Y = 100;
+
+const char* W_NAME = "Inverse Kinematics";
+
+#define _USE_MULTISAMPLE
+constexpr auto MULTISAMPLE_LEVEL = 8;
